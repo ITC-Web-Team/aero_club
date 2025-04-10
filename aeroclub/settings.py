9e2cf51f07fc3b186a003120e2c2c8ebcb2360c8
deepak silaych <deepaksilaych@gmail.com>
2025-04-10 11:41:26 +0530

    fix error

--- aeroclub/settings.py
+++ aeroclub/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'gu'
+    'gunicorn'
 ]
 
 ROOT_URLCONF = 'aeroclub.urls'
